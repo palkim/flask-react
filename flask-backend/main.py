8aae84e3e8d67a46e0e873b048589328dee6bb5e
palkim <31888413+palkim@users.noreply.github.com>
2020-07-13 14:18:20 +0300

    Update main.py

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -21,6 +21,7 @@ app = flask.Flask("__main__")
 @app.route("/")
 def my_index():
     # returns the index.html file from templates folder with token variable which includes the sql query result
+    # return flask.render_template("index.html", token=result) 
     return flask.render_template("index.html", token="Hello Flask+React!")
 
 
