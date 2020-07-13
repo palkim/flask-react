c4ba324f70428eeb1f4ef839f92bb48bb1eef780
palkim <31888413+palkim@users.noreply.github.com>
2020-07-13 14:09:43 +0300

    Update App.js

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import garfield from "./garfield.png"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <img src={garfield} alt="have some fun"/>
         <p>
           Below token came from flask:
         </p>
