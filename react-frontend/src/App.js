1d8b0dec95320cbd0c61e2b3cd94f29a34d61ab8
palkim <31888413+palkim@users.noreply.github.com>
2020-07-13 14:43:55 +0300

    Update App.js

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <a>
           Below token came from flask:
-        </p>
+        </a>
           <p>{window.token}</p>
       </header>
     </div>
